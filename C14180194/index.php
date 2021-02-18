e4dd94e3c2898cbbc63fa25f62ceb1272826afe1
Felic <felicfingki@yahoo.com>
2021-02-18 11:56:54 +0700

    C14180194-D

--- C14180194/index.php
+++ C14180194/index.php
@@ -1,2 +1,15 @@
 <p>Felic Cahyadi</p>
 <h2>C14180194</h2>
+<h3>Date </h3>
+<input type="date" id="tanggal" name="tanggal">
+<button type="button" onclick="Hitung()">Hitung</button>
+<p id="hasilhitung">Hasil Perhitungan : </p>
+<script>
+        function Hitung() 
+        {
+            var tanggal = new Date (document.getElementById("tanggal").value);
+            var tanggal2 = new Date();
+            var result = Math.floor((tanggal - tanggal2) / (1000 * 60 * 60 * 24));
+            document.getElementById("hasilhitung").innerHTML = " Hasil Perhitungan : " + result;
+        }
+</script>
